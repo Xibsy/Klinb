bd8ced718ce9b48ed629f500a882813d0de4499c
Xibsy <pro45951@gmail.com>
2026-04-26 14:18:01 +0500

    bugs fix

--- data/utilities/friend_to_point.py
+++ data/utilities/friend_to_point.py
@@ -1,6 +1,11 @@
-def friend_to_point(friend):
-    return {
+def friend_to_point(friend) -> dict | None:
+    try:
+        point = {
         'pos': list(map(float, friend.geo_position.split(','))),
         'name': friend.name,
         'avatar': friend.avatar,
-    }
+        }
+        return point
+    except AttributeError:
+        return None
+
